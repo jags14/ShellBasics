6e0e401c3b456e0ef1136311f2c2411d9f250c8f
Jagpat Singh <jsingh14192@gmail.com>
2024-03-22 01:00:08 +0530

    week2 wip

---
+++ march20.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "hello world" > march20.txt
---
+++ theorem.sh
@@ -0,0 +1,4 @@
+    #!/bin/sh
+    echo file_{a..z}{a..z}{1..4}.txt > jagpat.txt
+    echo "Done creating 26*26*4 number of empty text files by just a single shell command"
+
